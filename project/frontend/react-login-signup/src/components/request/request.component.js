f1800d5f9f28d89d47a3716d5a27dfc8f1e80864
Imre Toth <imretoth1990@gmail.com>
2022-11-24 20:43:47 +0100

    Add: useEffect to request.component

--- project/frontend/react-login-signup/src/components/request/request.component.js
+++ project/frontend/react-login-signup/src/components/request/request.component.js
@@ -1,40 +1,9 @@
-// import React from "react";
-
-// class GetRequest extends React.Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       totalReactPackages: null,
-//     };
-//   }
-
-//   componentDidMount() {
-//     // Simple GET request using fetch
-//     fetch("http://localhost:8080/api/private")
-//       .then((response) => response.json())
-//       .then((data) => this.setState({ totalReactPackages: data.total }))
-//       .then((data) => console.dir(data));
-//   }
-
-//   render() {
-//     const { totalReactPackages } = this.state;
-//     return (
-//       <div className="card text-center m-3">
-//         <h5 className="card-header">Simple GET Request</h5>
-//         <div className="card-body">Total react packages: {totalReactPackages}</div>
-//       </div>
-//     );
-//   }
-// }
-
-// export { GetRequest };
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function GetRequest() {
   const [state, setState] = useState("Loading data...");
 
-  function fetchData() {
+  useEffect(() => {
     fetch("http://localhost:8080/api/private", {
       method: "GET",
       headers: {
@@ -44,17 +13,16 @@ function GetRequest() {
       },
     })
       .then((response) => response.json())
+      //   .then((response) => console.log(response))
       .then((data) => console.dir(data))
       .then((data) => setState(data));
-  }
-
-  fetchData();
+  }, []);
 
   return (
     <>
       <div className="card text-center m-3">
         <h5 className="card-header">Simple GET Request</h5>
-        <div className="card-body">Total react packages: {state}</div>
+        <div className="card-body">Total react packages: </div>
       </div>
     </>
   );
